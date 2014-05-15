3fb52971d0b77a4592227b41dae8e2a6c412a08e
Tim Koopmans <tim.koops@gmail.com>
2014-05-15 15:20:22 +1000

    no favicons

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,34 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @products }
+    end
   end
 
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @product }
+    end
   end
 
   def new
     @product = Product.new
+    respond_to do |format|
+      format.html
+      format.json { render json: @product }
+    end
   end
 
   def edit
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @product }
+    end
   end
 
   def create
